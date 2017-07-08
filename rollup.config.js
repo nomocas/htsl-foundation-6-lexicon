7b8ec486f2e956897455833ec974803aa95b50c8
Gilles Coomans <gilles.coomans@gmail.com>
2017-07-08 14:55:40 +0200

    fix(build): update rollup plugins and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 const pkg = require('./package.json');
 const external = Object.keys(pkg.dependencies);
 
 export default {
 	entry: 'src/index.js',
-	plugins: [babel(babelrc())],
+	plugins: [babel(babelrc()), nodeResolve({}), commonjs()],
 	external,
 	targets: [{
 		dest: pkg.main,
